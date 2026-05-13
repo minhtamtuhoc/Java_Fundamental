c51b1a776fd061bff59c24803000911ddc23a1be
minhtamtuhoc <minhtamhmp@gmail.com>
2026-05-13 08:50:59 +0700

    Add files via upload

---
+++ Session07/Gioi2/Book.java
@@ -0,0 +1,14 @@
+package session07;
+
+public class Book {
+
+    String title;
+    String author;
+    double price;
+
+    public void printInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
